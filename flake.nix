9358970541b5a7f71bafdef5027e6d56f077806f
Joshua Perry <josh@6bit.com>
2024-02-27 13:05:12 -0700

    Update flake for kustomize 5.3

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv";
     nix2container.url = "github:nlewo/nix2container";
     nix2container.inputs.nixpkgs.follows = "nixpkgs";
@@ -17,6 +18,15 @@
       systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
 
       perSystem = { config, self', inputs', pkgs, system, ... }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            (final: prev: {
+              unstable = inputs.nixpkgs-unstable.legacyPackages."${system}";
+            })
+          ];
+          config = { };
+        };
 
         devenv.shells.default = {
           languages.javascript.enable = true;
@@ -30,7 +40,8 @@
           packages = with pkgs;[
             k9s
             kubectl
-            kustomize
+            # Need 5.3 for kubeVersion in helmchartinflator
+            unstable.kustomize
             kubernetes-helm
             (google-cloud-sdk.withExtraComponents [
               google-cloud-sdk.components.gke-gcloud-auth-plugin
